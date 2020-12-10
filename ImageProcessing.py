17d936f6b16755fa6f4a1cedb47e810ee6474f0d
Samuel Evans <Sevans@bu.edu>
2020-12-10 14:33:11 -0500

    [ADD] dataset3 processing functions

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -89,13 +89,17 @@ def show_rgb(imgdata, figsize=(10,2), pad=0.1):
 
 
 ### Put images into dict with pokemon names as keys! ###
-def get_image_and_poke(filename, folder=dp.IMAGESFOLDER):
+def get_image_and_poke(filename, folder=dp.IMAGESFOLDER, verbose=True):
     '''returns [imagedata, pokemonname]. e.g. filename='squirtle.png' -> returns [image, 'squirtle'].'''
     name, ext = filename.split('.')
     ext = '.' + ext
-    return [get_image(name, folder=folder, ext=ext), name]
+    try:
+        return [get_image(name, folder=folder, ext=ext), name]
+    except:
+        if verbose: print('failed to get image:',filename)
+        return None
 
-def get_all_images(folder, imnames=None, verbose=True):
+def get_all_images(folder, imnames=None, verbose=True, to_rgb=True):
     '''returns dict of items pokemon:image. e.g. result['squirtle'] = image of squirtle.
     imnames: None or list of strings
         None -> default to all files in folder
@@ -104,10 +108,14 @@ def get_all_images(folder, imnames=None, verbose=True):
     now=time.time()
     allimagenames = imnames if imnames is not None else os.listdir(folder)
     #should include some line which skips non-images here. (not needed for this data though.)
-    temp = [get_image_and_poke(name, folder=folder) for name in allimagenames]
-    allimages = {t[1]: t[0] for t in temp}
-    if verbose: print('got all {:3d} images in {:5.2} seconds (from folder = {:s})'.format(
-                     len(allimagenames), time.time()-now, folder))
+    temp = []
+    for name in allimagenames:
+        gip = get_image_and_poke(name, folder=folder, verbose=verbose)
+        if gip is not None: temp += [gip]
+    to_rgb_fn =   img_to_rgb   if   to_rgb   else  lambda x: x
+    allimages = {t[1]: to_rgb_fn(t[0]) for t in temp}
+    print('got all {:3d} images in {:5.2f} seconds (from folder = {:s})'.format(
+            len(allimagenames), time.time()-now, folder))
     return allimages
 
 ### Some images were jpgs, others were pngs. Convert all to jpgs. ###
@@ -138,7 +146,7 @@ def plt_rescaled(img, **imshow_kwargs):
     '''plots scaled img; rather than clipping values not in [0,1], makes all values fit in [0,1].'''
     plt.imshow(rescaled(img, [0,1]), **imshow_kwargs)
 
-### Begin working with scond image dataset (70MB) ###
+### Begin working with second image dataset (70MB) ###
 # these images tend to be named X.png with X a number (e.g. 1.png for bulbasaur)
 # or named X-qualifiers.png with qualifiers a word or set of words separated by '-' (e.g. 'mega-y' or 'winter')
 
@@ -165,3 +173,78 @@ def resize_images(ims, shape, verbose=True):
     return result
 
 #PIL.Image.fromarray(np.uint8(ip.rescaled(im, [0,255]))).resize((256, 256))
+        
+        
+## Begin working with third image dataset (1.21GB), images of first 150 pokes only, multiple images of each. ##
+# images are in folders which are named by pokemon names. E.g. ./Abra/ has many pictures of abra.
+        
+def get_some_images_and_reshape(folder, some=50, shape=(240, 240), imnames=None,
+                                verbose=True, printfreq=5,                       #printfreq in seconds
+                                random=True, to_rgb=True):
+    '''returns np.array of some images from folder.
+    some = max number of images to get from folder.
+    shape = shape to make image channels.
+    imnames = image names to use (will os.listdir(folder) if imnames is None).
+    verbose = whether to print when there are errors with image reading.
+    printfreq = amount of time (in seconds) between progress updates.
+    random = whether to choose images randomly (if False, read them in order).
+    to_rgb = whether to output images in rgb. (to_rgb=False is not implemented yet.)
+    '''
+    now=time.time()
+    allimagenames = imnames if imnames is not None else os.listdir(folder)
+    if random: allimagenames = np.random.permutation(allimagenames)
+    #should include some line which skips non-images here. (not needed for this data though.)
+    to_rgb_fn =  img_to_rgb   if   to_rgb   else  lambda x: x
+    
+    ## begin main loop for function ##
+    printtime = now + printfreq
+    imagelist = []
+    i = 0
+    while len(imagelist)<some and i<len(allimagenames):
+        if time.time() > printtime:
+            print('.. getting image {:2d}.'.format(i), end='')
+            printtime += printfreq
+        gip = get_image_and_poke(allimagenames[i], folder=folder, verbose=verbose)
+        if gip is not None:
+            if len(gip[0].shape)==3:     #check to ensure image has channels (for some reason some dont have channels).
+                imagelist += [resize_image(to_rgb_fn(gip[0]), shape)]
+        i += 1
+    allimages = np.array(imagelist)
+    ## end main loop for function ##
+    
+    if verbose: print('Got {:3d} of the {:3d} images in {:5.2f} seconds (from folder = {:s}).'.format(
+                        len(imagelist), len(allimagenames), time.time()-now, folder))
+    return allimages
+
+def _print_clear(N=80):
+    '''clears current printed line of up to N characters, and returns cursor to beginning of the line.
+    debugging: make sure to use print(..., end=''), else your print statement will go to the next line.
+    '''
+    print('\r'+' '*80+'\r',end='')
+
+def images_from_folders_in_dir(directory, Nf=None, **kwargs):
+    '''returns dict of images from folders in directory, with keys=folder names.
+    Nf = max number of folders to get images from. Mainly useful when debugging.
+    **kwargs: get passed to get_some_images_and_reshape. see that function for more info.
+    '''
+    now=time.time()
+    some = kwargs.pop('some', 10)
+    verbose = kwargs.pop('verbose', False)
+    print('Loading up to {:2d} images from each folder. Set kwarg "some" to another value to change this number.'.format(some))
+    folders = sorted([folder for folder in os.listdir(directory) if os.path.isdir(os.path.join(directory, folder))])
+    result = dict()
+    for folder in folders[:Nf]:
+        print('Current folder:',folder, end='')
+        result[folder] = get_some_images_and_reshape(os.path.join(directory, folder), some=some, verbose=verbose, **kwargs)
+        _print_clear(N=30)
+    print('Finished in {:5.2f} seconds.'.format(time.time()-now))
+    return result
+    
+def flatten_imlists(imlists):
+    '''once-flatten list of lists of images to a single list of images.
+    return (once-flattened list), (idx correponding to list for original list).
+    e.g. [[A1,A2], [B1,B2,B3], [C1]] --> ([A1,A2,B1,B2,B3,C1], [0,0,1,1,1,2])
+    '''
+    idx  = [     i        for i in range(len(imlists)) for j in range(len(imlists[i]))]
+    flat = [imlists[i][j] for i in range(len(imlists)) for j in range(len(imlists[i]))]
+    return flat, idx
